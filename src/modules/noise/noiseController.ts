d7423329fb498a576730bc840afe0d91ac80caa1
maiswan <173935047+maiswan@users.noreply.github.com>
2025-01-25 10:28:31 +0800

    Remove brightness option from NoiseController Use BackdropFilterController instead

--- src/modules/noise/noiseController.ts
+++ src/modules/noise/noiseController.ts
@@ -4,50 +4,25 @@ import { ControllerBase } from '../controllerBase';
 export class NoiseController extends ControllerBase {
     public readonly Identifier = "noise";
 
-    private readonly baseTag = `${this.Identifier}_base`;
     private readonly noiseTag = `${this.Identifier}_noise`;
-    private readonly brightnessTag = `${this.Identifier}_brightness`;
 
     private _canvasElement: HTMLElement;
     private _noiseElement: HTMLElement = document.createElement("div");
-    private _brightnessElement: HTMLElement= document.createElement("div");
 
     private _noise: number;
     public get noise(): number { return this._noise; }
     public set noise(value: number)
     { 
         this._noise = value;
-        this.update(this.noise, this.brightness);
+        this.update(this.noise);
     }
 
-    private _brightness : number;
-    /**
-     * @deprecated set brightness through BackdropFilter instread
-     */
-    public get brightness(): number { return this._brightness; }
-    /**
-     * @deprecated set brightness through BackdropFilter instread
-     */
-    public set brightness(value: number)
-    { 
-        console.warn("NoiseController.Brightness is deprecated. Use BackdropFilterController instead.")
-        this._brightness = value;
-        this.update(this.noise, this.brightness);
-    }
-
-    constructor(canvas: HTMLElement, noise: number, brightness?: number)
+    constructor(canvas: HTMLElement, noise: number)
     {
         super();
         this._canvasElement = canvas;
         this.noise = noise;
 
-        this._brightness = brightness ?? 1;
-        
-        this._brightnessElement.classList.add(this.baseTag);
-        this._brightnessElement.id = this.brightnessTag;
-        this._canvasElement.appendChild(this._brightnessElement);
-
-        this._noiseElement.classList.add(this.baseTag);
         this._noiseElement.id = this.noiseTag;
         this._canvasElement.appendChild(this._noiseElement);
     }
@@ -55,13 +30,13 @@ export class NoiseController extends ControllerBase {
     enable(): void
     {
         super.enable();
-        this.update(this.noise, this.brightness);
+        this.update(this.noise);
     }
 
     disable(): void
     {
         super.disable();
-        this.update(0, 1);
+        this.update(0);
     }
 
     toggle(): void
@@ -69,10 +44,10 @@ export class NoiseController extends ControllerBase {
         super.toggle();
     }
 
-    update(noise: number, brightness: number)
+    update(noise: number)
     {
-        document.documentElement.style.setProperty(`--${this.noiseTag}`, "" + noise);   
-        document.documentElement.style.setProperty(`--${this.brightnessTag}`, "" + brightness);  
+        // document.documentElement.style.setProperty(`--${this.noiseTag}`, "" + noise);
+        this._noiseElement.style.setProperty(`--${this.noiseTag}`, "" + noise);
     }
 
     receive(parameters: string[]): void {
@@ -82,10 +57,6 @@ export class NoiseController extends ControllerBase {
         {
             case "setNoise":
                 this.noise = Number(parameters[1]);
-                return;
-
-            case "setBrightness":
-                this.brightness = Number(parameters[1]);
                 return;
         }
     }
